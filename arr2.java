a64257f1b3543762385f15ceb2f0bbc196ba89bc
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-16 15:20:01 +0400

    Update arr2.java

--- arr2.java
+++ arr2.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-	public class arr2{
-		public static void main(String[] arg){
-			int[] a;
-			int b;
-			Scanner sc=new Scanner(System.in);	
-			System.out.print("Enter numbers of erray:");
-			b=sc.nextInt();
-			a=new int[b];
-			for (int i=0; i<b; i++) {
-				System.out.print("Enter a["+ i +"] =");
-				a[i]=sc.nextInt();
-			}
-	}
+public class arr2 {
+  public static void main(String[] arg) {
+    int[] a;
+    int b;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter numbers of erray:");
+    b = sc.nextInt();
+    a = new int[b];
+    for (int i = 0; i < b; i++) {
+      System.out.print("Enter a[" + i + "] =");
+      a[i] = sc.nextInt();
+    }
+  }
 }
